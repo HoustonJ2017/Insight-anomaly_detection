fbbb116da98743f00a7ca95fcf0114e2233a3fd7
HoustonJ2017 <jerome198246@gmail.com>
2017-07-04 11:17:31 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python ./src/process_log.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
+#python ./src/process_log.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
+python ./src/process_log.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json  1  15 
+## Optional pass D and T to the code default is D = 1, T = 10 
